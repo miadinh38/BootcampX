33f2e7b73820f3749267fc7913f8afd9e282399b
Mia Dinh <miadinh38@gmail.com>
2023-11-06 01:29:47 -0500

    Fix spelling error in assistance_requests table and Get the total number of assistance_requests for a teacher

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -8,12 +8,12 @@ CREATE TABLE teachers (
 
 CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
   started_at TIMESTAMP,
-  complete_at TIMESTAMP,
+  completed_at TIMESTAMP,
   student_feedback TEXT,
   teacher_feedback TEXT
 );
