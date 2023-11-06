15de49d7da8dfb8b19ba40ee69a3d93e8a97e31e
Mia Dinh <miadinh38@gmail.com>
2023-11-06 01:40:53 -0500

    Get the total number of assistance_requests for a student

--- 4_queries/1_total_teacher_assistance-requests.sql
+++ 4_queries/1_total_teacher_assistance-requests.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(assistance_requests.*) as total_assistances, teachers.name as name
 FROM assistance_requests
-JOIN teachers ON teachers.id = teacher_id
+JOIN studens ON students.id = student_id
 WHERE name = 'Waylon Boehm'
 GROUP BY teachers.name;
 
